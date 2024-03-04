c343ab816cefad18f0653fcad1571068225abbf1
minhtq05 <minhtq2005@gmail.com>
2024-03-04 18:38:59 -0500

    modified + add more feature to linkedin scrapper1

--- src/linkedin_scrapper.py
+++ src/linkedin_scrapper.py
@@ -1,4 +1,5 @@
 import base64
+from msg_handlers import proceed
 
 
 def linkedin_job_alert(service, jobs):
@@ -26,46 +27,19 @@ def linkedin_job_alert(service, jobs):
 
 
 def linkedin_list_jobs(bot, message, job_items):
-    rep, rf = '', 0
+    rep = ''
     count = 0
-    for job in job_items:
-        # if count % 3 == 0:
-        #     bot.send_message(
-        #         message.chat.id, 'Sir, do you like me to send you more?')
-
-        #     cont = False
-
-        #     @bot.message_handler(commands=['yes', 'no'])
-        #     def proceed_job_alerts(bot, message):
-        #         global cont
-        #         text = message.text.lower()
-        #         false_count = 0
-        #         while True:
-        #             if text[:3] == 'yes' or text[:4] == 'yeah':
-        #                 bot.reply_to(
-        #                     message, 'Thank you, sir. I will keep sending you more right now.')
-        #                 cont = True
-        #                 return
-        #             elif text[:2] == 'no' or text[:3] == 'nah':
-        #                 bot.reply_to(
-        #                     message, 'Thank you, sir. I will stop now.')
-        #                 cont = False
-        #                 return
-        #             else:
-        #                 if (false_count == 2):
-        #                     bot.reply_to(
-        #                         message, 'I still didn\'t get that, sir. But I will count that as a no and stop now.')
-        #                     cont = False
-        #                     return
-        #                 bot.reply_to(
-        #                     message, 'I didn\'t get that, sir. Please type \'yes\' or \'no\'.')
-        #                 false_count += 1
-        #     if cont == False:
-        #         return
+    for i, job in enumerate(job_items):
+        if count == 3:
+            sent_msg = bot.send_message(
+                message.chat.id, 'Sir, do you like me to send you more?')
+            bot.register_next_step_handler(
+                sent_msg, proceed, bot=bot, return_func=linkedin_list_jobs, job_items=job_items[i:])
+            return
         text = ''
         link = job[-1][10:]
         text += f'<a href=\'{link}\'>{job[0]}</a>\n' + \
-            f'<strong>{job[1]}</strong>\n'
+            f'<b>{job[1]}</b>\n'
         for i in job[2:3]:
             text += f'{i}\n'
         # debug('text === ', text)
@@ -78,3 +52,47 @@ def linkedin_list_jobs(bot, message, job_items):
     if rep != '':
         bot.send_message(
             message.chat.id, rep, parse_mode='HTML', disable_web_page_preview=True)
+
+
+# def proceed(message, bot, job_items):
+#     text = message.text.lower()
+#     for i in range(3):
+#         if text[:2] == 'ye':
+#             bot.reply_to(
+#                 message, 'Thank you, sir. I will keep sending you more right now.')
+#             linkedin_list_jobs(bot, message, job_items)
+#             return
+#         elif text[:2] == 'no' or text[:3] == 'nah':
+#             bot.reply_to(
+#                 message, 'Thank you, sir. I will stop now.')
+#             return
+#         else:
+#             if i == 2:
+#                 bot.reply_to(
+#                     message, 'I still didn\'t get that, sir. But I will count that as a no and stop now.')
+#                 cont = False
+#                 return
+#             bot.reply_to(
+#                 message, 'I didn\'t get that, sir. Please type \'yes\' or \'no\'.')
+
+
+# def proceed(message, bot, return_func, *args, **kwargs):
+#     text = message.text.lower()
+#     for i in range(3):
+#         if text[:2] == 'ye':
+#             bot.reply_to(
+#                 message, 'Thank you, sir. I will keep sending you more right now.')
+#             return_func(bot, message, *args, **kwargs)
+#             return
+#         elif text[:2] == 'no' or text[:3] == 'nah':
+#             bot.reply_to(
+#                 message, 'Thank you, sir. I will stop now.')
+#             return
+#         else:
+#             if i == 2:
+#                 bot.reply_to(
+#                     message, 'I still didn\'t get that, sir. But I will count that as a no and stop now.')
+#                 cont = False
+#                 return
+#             bot.reply_to(
+#                 message, 'I didn\'t get that, sir. Please type \'yes\' or \'no\'.')
